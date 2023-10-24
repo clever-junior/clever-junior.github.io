17f72b4090347d0f3adc99e7771c3598ab9b6676
clever-junior <tk_clever.junior@outlook.com>
2023-10-24 11:15:04 -0400

    refactor: :art: remove unused dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import './assets/main.css'
+import './assets/dist/output.css'
 
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+const pinia = createPinia()
 const app = createApp(App)
+
+app.use(pinia)
 
 app.use(router)
 
