c0a8beff31ee52882b012f42bab0ca8d30968cd3
clever-junior <tk_clever.junior@outlook.com>
2023-10-25 00:55:13 -0400

    feat: :sparkles: add hero

--- src/App.vue
+++ src/App.vue
@@ -10,17 +10,17 @@ const handleMenu = () => {
 </script>
 
 <template>
-  <header class="flex justify-center items-center w-full h-16 bg-blue-950 text-white">
-    <div class="flex items-center w-11/12 max-sm:justify-between mt-2 ml-2">
-      <h1 class="font-bold text-4xl pb-3">Clever Junior</h1>
+  <header class="flex justify-center items-center w-full h-16">
+    <div class="flex items-center w-10/12 justify-between mt-2 ml-2">
+      <h1 class="font-semibold text-2xl pb-3">Clever Junior</h1>
       
-      <section v-bind:class="showMenu ? 'max-sm:bg-blue-950 max-sm:absolute max-sm:top-0 max-sm:left-0 max-sm:z-10 max-sm:flex max-sm:justify-center max-sm:items-center max-sm:w-full max-sm:h-full' : 'ml-14'">
+      <section v-bind:class="showMenu ? 'max-sm:absolute max-sm:top-0 max-sm:left-0 max-sm:z-10 max-sm:flex max-sm:justify-center max-sm:items-center max-sm:w-full max-sm:h-full' : 'ml-14'">
         <button type="button" v-on:click="handleMenu" class="sm:hidden w-9 h-8" v-bind:class="showMenu && 'absolute right-4 top-4'"> 
-          <div v-bind:class="showMenu && 'rotate-45 translate-x-2 translate-y-2 transition-all'" class="bg-white h-2 w-full my-1 mx-auto rounded-full"></div>
-          <div v-bind:class="showMenu && 'opacity-0 transition-all'" class="bg-white h-2 w-full my-1 mx-auto rounded-full"></div>
-          <div v-bind:class="showMenu && 'transition-all -rotate-45 translate-x-2 -translate-y-4'" class="bg-white h-2 w-full my-1 mx-auto rounded-full"></div>
+          <div v-bind:class="showMenu && 'rotate-45 translate-x-2 translate-y-2 transition-all'" class="bg-black h-2 w-full my-1 mx-auto rounded-full"></div>
+          <div v-bind:class="showMenu && 'opacity-0 transition-all'" class="bg-black h-2 w-full my-1 mx-auto rounded-full"></div>
+          <div v-bind:class="showMenu && 'transition-all -rotate-45 translate-x-2 -translate-y-4'" class="bg-black h-2 w-full my-1 mx-auto rounded-full"></div>
         </button>
-        <nav v-bind:class="showMenu ? 'max-sm:block text-center transition-all text-4xl' : 'max-sm:hidden'">
+        <nav v-bind:class="showMenu ? 'max-sm:bg-white w-full h-full max-sm:block text-center transition-all text-4xl' : 'max-sm:hidden'">
           <ul class="flex gap-3 font-medium text-lg" v-bind:class="showMenu && 'max-sm:text-center max-sm:block max-sm:transition-all max-sm:text-4xl'">
             <li>
               <RouterLink to="/" v-bind:class="showMenu && 'block transition-all text-4xl'">Home</RouterLink>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,21 @@
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+    <section class="mt-20 w-full items-center flex justify-center">
+      <div class="w-10/12">
+        <div>
+          <h1 class="text-4xl font-bold ">
+            Fullstack Javascript Web Developer
+          </h1>
+          <p class="">A passionate software engineer from Brazil. If you want to connect with me you can find me on</p>
+        </div>
+        <div class="mt-3">
+          <a class=" bg-blue-500 text-white p-3 rounded-full" href="https://www.linkedin.com/in/clever-lopes/" target="_blank">Linkedin</a>
+          <a class=" bg-black text-white p-3 rounded-full" href="https://github.com/clever-junior/" target="_blank">Github</a>
+        </div>
+      </div>
+    </section>
+  <main class="">
+
   </main>
 </template>
